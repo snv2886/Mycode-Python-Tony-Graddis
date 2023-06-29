ddf0805aa89678fe21d74a41281f1d8a90e43585
snv2886 <snv2886@gmail.com>
2023-06-29 23:13:30 +0300

    updated ex_python-6.py

--- ex_python-6.py
+++ ex_python-6.py
@@ -11,18 +11,19 @@
 
 
 # 6.2.2
-def main():
-    total = 0
-    COUNT = 5
-    filename = open(input('Enter file name: '), 'r')
-    content = filename.readline()
-    while content != '' and total < COUNT:
-        total += 1
-        content = content.rstrip('\n')
-        print(f'{total}: {content}')
-        content = filename.readline()
-    filename.close()
+# def main():
+#     total = 0
+#     COUNT = 5
+#     filename = open(input('Enter file name: '), 'r')
+#     content = filename.readline()
+#     while content != '' and total < COUNT:
+#         total += 1
+#         content = content.rstrip('\n')
+#         print(f'{total}: {content}')
+#         content = filename.readline()
+#     filename.close()
+#
+#
+# if __name__ == "__main__":
+#     main()
 
-
-if __name__ == "__main__":
-    main()
