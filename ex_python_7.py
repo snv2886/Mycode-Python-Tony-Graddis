4827e05c3a8d26e0f2a9fdcb9e1e7f0de8c04688
snv2886 <snv2886@gmail.com>
2023-07-06 16:40:21 +0300

    new: ex_python_7.py

---
+++ ex_python_7.py
@@ -0,0 +1,26 @@
+# mylist = [10, 20, 30, 40]
+# # print(mylist[0], mylist[1], mylist[2], mylist[3])
+# index = 0
+# while index < 4:
+#     print(mylist[index])
+#     index += 1
+# mylist = [10, 20, 30, 40]
+# print(mylist[-1], mylist[-2], mylist[-3], mylist[-4])
+# index = -1
+# while index > -5:
+#     print(mylist[index])
+#     index += -1
+# index = 0
+# while index < 5:
+#     print(mylist[index])
+#     index += 1
+
+# mylist = [10, 20, 30, 40]
+# index = 0
+# while index < len(mylist):
+#     print(mylist[index])
+#     index += 1
+
+names = ['Jenny', 'Kelly', 'Chloya', 'Obry']
+for index in range(len(names)):
+    print(names[index])
